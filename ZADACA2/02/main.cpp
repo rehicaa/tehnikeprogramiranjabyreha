84f7cefa1857fd833fa5f061d5159f818ac8f6ca
rehicaa <rehapremium@gmail.com>
2026-05-08 15:40:50 +0100

    Zad2 promijenjene

--- ZADACA2/02/main.cpp
+++ ZADACA2/02/main.cpp
@@ -2,11 +2,16 @@
 #include <cmath>
 #include <stdexcept>
 #include <algorithm>
+#include <limits>
 
-using std::cout, std::cin, std::endl, std::domain_error, std::min;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::domain_error;
+using std::min;
 
 void SumaCetiriKvadrata(int n, int &a, int &b, int &c, int &d) {
-    if (n < 0) throw domain_error("Broj koji se rastavlja mora biti nenegativan");
+    if (n < 0) throw domain_error("");
 
     int korijen = std::sqrt(n);
     for (int i = korijen; i >= 0; i--) {
@@ -19,7 +24,7 @@ void SumaCetiriKvadrata(int n, int &a, int &b, int &c, int &d) {
 
             for (int k = min(j, (int)std::sqrt(r2)); k >= 0; k--) {
                 int r3 = r2 - k * k;
-                int l = std::sqrt(r3);
+                int l = std::round(std::sqrt(r3));
                 if (l * l == r3 && l <= k) {
                     a = i; b = j; c = k; d = l;
                     return;
@@ -31,15 +36,26 @@ void SumaCetiriKvadrata(int n, int &a, int &b, int &c, int &d) {
 
 int main() {
     int n;
-    while (cout << "Unesite prirodan broj: ", cin >> n) {
-        try {
-            int a, b, c, d;
-            SumaCetiriKvadrata(n, a, b, c, d);
-            cout << n << "=" << a << "^2+" << b << "^2+" << c << "^2+" << d << "^2" << endl;
-        } catch (domain_error &e) {
-            cout << "Uneseni broj nije prirodan broj!" << endl;
+    for (;;) {
+        cout << "Unesite prirodan broj: ";
+        if (!(cin >> n)) {
+            if (cin.eof()) break;
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
         }
-        cout << endl;
+
+        if (n <= 0) {
+            cout << "Uneseni broj nije prirodan broj!" << endl;
+        } else {
+            try {
+                int a, b, c, d;
+                SumaCetiriKvadrata(n, a, b, c, d);
+                cout << n << "=" << a << "^2+" << b << "^2+" << c << "^2+" << d << "^2" << endl << endl;
+            } catch (...) {
+                cout << "Uneseni broj nije prirodan broj!" << endl;
+            }
+        }
     }
     return 0;
 }
